64c627499de6993e4a97e642bc6ef266ba332ac6
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2022-03-09 20:08:41 +0900

    220309

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanjung <hanjung@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 17:09:59 by hanjung           #+#    #+#             */
+/*   Updated: 2022/03/09 17:10:01 by hanjung          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t num)
+{
+	size_t	i;
+
+	i = 0;
+	if (!num || dest == src)
+		return (dest);
+	if (!dest && !src)
+		return (NULL);
+	while (i < num)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
