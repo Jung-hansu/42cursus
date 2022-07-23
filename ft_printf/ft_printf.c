467c7d98ac79aee0aee8d0cb176e2826ed9617d3
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2022-07-23 17:46:11 +0900

    20220723

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,41 +6,48 @@
 /*   By: hanjung <hanjung@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 15:21:18 by hanjung           #+#    #+#             */
-/*   Updated: 2022/05/22 18:13:43 by hanjung          ###   ########.fr       */
+/*   Updated: 2022/07/23 17:45:44 by hanjung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	set_form(const char **s, va_list ap)
+static int	set_form(const char *s, va_list ap)
 {
-	if (!(*s))
+	if (!s)
 		return (-1);
-	if (**s == '#')
-		
+	while (*s != DTYPE)
+	{
+
+	}
 }
 
 int	ft_printf(const char *s, ...)
 {
-	va_list	ap;
-	int		res_len;
+	int		ret;
 	int		read_form;
+	va_list	ap;
 
-	res_len = 0;
+	ret = 0;
 	va_start(ap, s);
 	while (*s)
 	{
-		if (*s != '%')
-			res_len += write(1, s++, 1);
+		if (*s != '%'){
+			ft_putchar_fd(*s, 1);
+			ret++;
+			s++;
+		}
 		else
 		{
-			
 			read_form = set_form(&s, ap); //format parsing 필요
 			if (read_form < 0)
-				return (-1);
-			res_len += read_form;
+			{
+				ret = -1;
+				break ;
+			}
+			ret += read_form;
 		}
 	}
 	va_end(ap);
-	return (res_len);
+	return (ret);
 }
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanjung <hanjung@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/23 16:26:02 by hanjung           #+#    #+#             */
+/*   Updated: 2022/07/23 17:45:42 by hanjung          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
@@ -7,15 +19,15 @@
 #include "libft/libft.h"
 
 typedef struct{
-    int sharp;
-    int zero;
-    int space;
-    int comma;
-    int plus;
-    int minus;
-    
-}
+	int sharp;
+	int zero;
+	int space;
+	int upper;
+	int plus;
+	int minus;
+}flag;
 
-int ft_printf(const char *format, ...);
+
+int	ft_printf(const char *s, ...);
 
 #endif
